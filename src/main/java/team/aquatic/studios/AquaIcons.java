eaba10eeec2c740996dc4c1e8ef361e5e665efcd
Sxntido <sxntido@gmail.com>
2024-07-13 02:58:31 -0300

    New prefix onEnable() - onDisable()

--- src/main/java/team/aquatic/studios/AquaIcons.java
+++ src/main/java/team/aquatic/studios/AquaIcons.java
@@ -1,5 +1,6 @@
 package team.aquatic.studios;
 
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -9,8 +10,14 @@ import team.aquatic.studios.commands.Executor;
 import team.aquatic.studios.commands.tab.ExcTab;
 import team.aquatic.studios.folder.Files;
 import team.aquatic.studios.listener.IconsListener;
+import team.aquatic.studios.tools.Utils;
 
 public final class AquaIcons extends JavaPlugin {
+
+    private String serverType;
+    String version = Bukkit.getVersion();
+    String pluginVersion = getDescription().getVersion();
+    String bukkitVersion = Bukkit.getBukkitVersion();
 
     public static AquaIcons instance;
     public static AquaIcons getInstance() {
@@ -33,7 +40,6 @@ public final class AquaIcons extends JavaPlugin {
     public void onEnable() {
         instance = this;
         int pluginId = 22572;
-        Metrics metrics = new Metrics(this, pluginId);
 
         icons = new Files("icons");
         config = new Files("config");
@@ -41,10 +47,24 @@ public final class AquaIcons extends JavaPlugin {
         RegisterCommands();
         RegisterListeners();
 
+        Bukkit.getConsoleSender().sendMessage(Utils.TranslateColor("&r"));
+        Bukkit.getConsoleSender().sendMessage(Utils.TranslateColor("&3     _   ___ "));
+        Bukkit.getConsoleSender().sendMessage(Utils.TranslateColor("&3    /_\\ |_ _|  &bAquaIcons &f(v" + pluginVersion + "&f) - &aEnabled"));
+        Bukkit.getConsoleSender().sendMessage(Utils.TranslateColor("&3   / _ \\ | |   &fRunning on " + bukkitVersion));
+        Bukkit.getConsoleSender().sendMessage(Utils.TranslateColor("&3  /_/ \\_\\___|"));
+        Bukkit.getConsoleSender().sendMessage(Utils.TranslateColor("            "));
+
+        Metrics metrics = new Metrics(this, pluginId);
     }
 
     @Override
     public void onDisable() {
+        Bukkit.getConsoleSender().sendMessage(Utils.TranslateColor("&r"));
+        Bukkit.getConsoleSender().sendMessage(Utils.TranslateColor("&c     _   ___ "));
+        Bukkit.getConsoleSender().sendMessage(Utils.TranslateColor("&c    /_\\ |_ _|  &bAquaIcons &f(v" + pluginVersion + "&f) - &cDisabled"));
+        Bukkit.getConsoleSender().sendMessage(Utils.TranslateColor("&c   / _ \\ | |   &fRunning on " + bukkitVersion));
+        Bukkit.getConsoleSender().sendMessage(Utils.TranslateColor("&c  /_/ \\_\\___|"));
+        Bukkit.getConsoleSender().sendMessage(Utils.TranslateColor("            "));
     }
 
     private void RegisterCommands() {
